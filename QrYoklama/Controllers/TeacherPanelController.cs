1392f02a41c6a749f0ee4d1674699952e6a0cc21
Adaliaqq <filizduskun1907@gmail.com>
2026-05-24 21:46:23 +0300

    de

--- QrYoklama/Controllers/TeacherPanelController.cs
+++ QrYoklama/Controllers/TeacherPanelController.cs
@@ -1,16 +1,46 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Security.Claims;
 
-namespace QRYoklamaSistemi.Controllers
+namespace QrYoklama.Controllers
 {
-    [Authorize] // Sadece giriş yapmış kullanıcılar girebilir
+    [Authorize(Roles = "Teacher")]
     public class TeacherPanelController : Controller
     {
         public IActionResult Index()
         {
-            // Giriş yapan hocanın bilgilerini çekebiliriz
-            var teacherEmail = User.Identity?.Name; 
-            return View();
+            var teacherName = User.Identity?.Name;
+            ViewBag.TeacherName = teacherName;
+
+            var dersler = new List<string> 
+            { 
+                "İnternet Programcılığı", 
+                "İçerik Yönetim Sistemi", 
+                "Görsel Programlama", 
+                "Sunucu İşletim Sistemi",
+                "Mesleki İngilizce" 
+            };
+
+            ViewBag.Derslikler = new List<string>
+            {
+                "Lab 1",
+                "Lab 2",
+                "Lab 3",
+                "Lab 4",
+                "Lab 5",
+                "Lab 6"
+            };
+
+            ViewBag.DersSaatleri = new List<string>
+            {
+                "08:15 - 10:00",
+                "10:15 - 12:00",
+                "13:15 - 15:00",
+                "15:15 - 17:00"
+            };
+
+            return View(dersler);
         }
     }
 }
--- QrYoklama/obj/Debug/net9.0/QRYoklama.AssemblyInfo.cs
+++ QrYoklama/obj/Debug/net9.0/QRYoklama.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QRYoklama")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f964da8d1485123ea554736a4f6601f81991bfa")]
 [assembly: System.Reflection.AssemblyProductAttribute("QRYoklama")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QRYoklama")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
